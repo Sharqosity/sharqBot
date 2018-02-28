cd230a89157be044c0b883d4b0c5669d32a1fc4c
Sharqosity <typingpalguy@gmail.com>
2018-02-27 23:28:40 -0500

    undid dropbox crap

--- src/main/java/sharqBot/Main.java
+++ src/main/java/sharqBot/Main.java
@@ -1,17 +1,10 @@
 package sharqBot;
 
-import com.dropbox.core.DbxException;
-import com.dropbox.core.DbxRequestConfig;
-import com.dropbox.core.v2.DbxClientV2;
-import com.dropbox.core.v2.files.ListFolderResult;
-import com.dropbox.core.v2.files.Metadata;
-import com.dropbox.core.v2.users.FullAccount;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.exceptions.RateLimitedException;
 import net.dv8tion.jda.core.requests.SessionReconnectQueue;
-import sharqBot.Message.JSONDude;
 import sharqBot.Message.Listener;
 import sharqBot.Message.SharqCoinListener;
 
@@ -44,26 +37,8 @@ public class Main {
             e.printStackTrace();
             System.exit(0);
         }
-        try {
-            in = new FileReader("./token/dropbox.txt");
-        } catch (FileNotFoundException e) {
-            System.out.println("Dropbox token not found!");
-            e.printStackTrace();
-            System.exit(0);
-        }
-        br = new BufferedReader(in);
-        String dropBoxToken = null;
-        try {
-            dropBoxToken = br.readLine();
-        } catch (IOException e) {
-            System.out.println("dropbox.txt read error!");
-            e.printStackTrace();
-            System.exit(0);
-        }
 
 
-
-        JSONDude.dropBoxToken = dropBoxToken;
 
 //        try {
 //            JSONDude.getFile("/ReflexServerBot/sharqcoin.json");
@@ -71,41 +46,8 @@ public class Main {
 //            e.printStackTrace();
 //        }
 
-        // Create Dropbox client
-        DbxRequestConfig config = new DbxRequestConfig("dropbox/java-tutorial", "en_US");
-        DbxClientV2 client = new DbxClientV2(config, dropBoxToken);
 
-        // Get current account info
-        FullAccount account = null;
-        try {
-            account = client.users().getCurrentAccount();
-        } catch (DbxException e) {
-            e.printStackTrace();
-        }
-        System.out.println(account.getName().getDisplayName());
 
-        // Get files and folder metadata from Dropbox root directory
-        ListFolderResult result = null;
-        try {
-            result = client.files().listFolder("");
-        } catch (DbxException e) {
-            e.printStackTrace();
-        }
-        while (true) {
-            for (Metadata metadata : result.getEntries()) {
-                System.out.println(metadata.getPathLower());
-            }
-
-            if (!result.getHasMore()) {
-                break;
-            }
-
-            try {
-                result = client.files().listFolderContinue(result.getCursor());
-            } catch (DbxException e) {
-                e.printStackTrace();
-            }
-        }
 
         JDABuilder jdaBuilder = new JDABuilder(AccountType.BOT);
         jdaBuilder.setReconnectQueue(new SessionReconnectQueue());
--- src/main/java/sharqBot/Message/JSONDude.java
+++ src/main/java/sharqBot/Message/JSONDude.java
@@ -1,35 +1,23 @@
 package sharqBot.Message;
 
-import com.dropbox.core.DbxException;
-import com.dropbox.core.DbxRequestConfig;
-import com.dropbox.core.v2.DbxClientV2;
-import com.dropbox.core.v2.files.FileMetadata;
 import net.dv8tion.jda.core.entities.User;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDateTime;
 
 public class JSONDude {
 
-    public static String dropBoxToken;
+
 
 
 
     public static JSONObject getUser(User targetUser) {
-        // Create Dropbox client
-        DbxRequestConfig config = new DbxRequestConfig("reflex-server-bot", "en_US");
-        DbxClientV2 client = new DbxClientV2(config, dropBoxToken);
-
         JSONParser parser = new JSONParser();
         try {
             Object obj = parser.parse(new FileReader("./sharqcoin.json"));
@@ -59,13 +47,6 @@ public class JSONDude {
 
 
 
-            try (InputStream in = new FileInputStream("./sharqcoin.json")) {
-                FileMetadata metadata = client.files().uploadBuilder("./ReflexServerBot/sharqcoin.json")
-                        .uploadAndFinish(in);
-            } catch (DbxException e) {
-                e.printStackTrace();
-            }
-
             return newUser;
         } catch (IOException | ParseException e) {
             e.printStackTrace();
@@ -74,99 +55,4 @@ public class JSONDude {
     }
 
 
-    public static void getFile(String foldername) {
-
-        try {
-
-            URL url = new URL("https://content.dropboxapi.com/2/files/download");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            String parameters = "{\"path\": \"" + foldername + "\"}";
-
-            conn.addRequestProperty ("Authorization", dropBoxToken);
-            conn.addRequestProperty ("Dropbox-API-Arg", parameters);
-            conn.setDoOutput(true);
-
-            if (conn.getResponseCode() != 200) {
-                System.out.println(conn.getResponseMessage());
-                throw new RuntimeException("Failed : HTTP error code : "
-                        + conn.getResponseCode());
-            }
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(
-                    (conn.getInputStream())));
-
-            String output;
-            System.out.println("Output from Server .... \n");
-            while ((output = br.readLine()) != null) {
-                System.out.println(output);
-            }
-
-            conn.disconnect();
-
-        } catch (IOException e) {
-
-            e.printStackTrace();
-
-        }
-
-    }
-
-    public static void putFile(String foldername, String path) {
-
-        try {
-
-            URL url = new URL("https://content.dropboxapi.com/2/files/upload");
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            String parameters = "{\"path\": \"" + foldername + "\"}";
-
-            conn.setRequestProperty("Content-Type", "application/octet-stream");
-            conn.addRequestProperty ("Authorization", dropBoxToken);
-            conn.addRequestProperty ("Dropbox-API-Arg", parameters);
-            conn.setRequestMethod("POST");
-
-
-            conn.setDoOutput(true);
-
-            Path pathFile = Paths.get(path);
-            byte[] data = Files.readAllBytes(pathFile);
-
-            DataOutputStream writer = new DataOutputStream(conn.getOutputStream());
-            /*
-            writer.writeBytes(parameters);
-            writer.flush();
-            */
-            writer.write(data);
-            writer.flush();
-
-            if (writer != null)
-                writer.close();
-
-            if (conn.getResponseCode() != 200) {
-                System.out.println(conn.getResponseMessage());
-                throw new RuntimeException("Failed : HTTP error code : "
-                        + conn.getResponseCode());
-            }
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(
-                    (conn.getInputStream())));
-
-            String output;
-            System.out.println("Output from Server .... \n");
-            while ((output = br.readLine()) != null) {
-                System.out.println(output);
-            }
-
-            conn.disconnect();
-
-        } catch (MalformedURLException e) {
-
-            e.printStackTrace();
-
-        } catch (IOException e) {
-
-            e.printStackTrace();
-
-        }
-
-    }
 }
