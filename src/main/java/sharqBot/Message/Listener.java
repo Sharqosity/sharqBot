e8e4dd6f7883ca075f3e80112681184119da7dfe
Sharqosity <typingpalguy@gmail.com>
2018-08-11 17:52:05 -0400

    Revert "merge timestamp in footer changes"
    This reverts commit dc42b11

--- src/main/java/sharqBot/Message/Listener.java
+++ src/main/java/sharqBot/Message/Listener.java
@@ -6,6 +6,7 @@ import net.dv8tion.jda.core.MessageBuilder;
 import net.dv8tion.jda.core.entities.*;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
+import net.dv8tion.jda.core.managers.GuildController;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -34,7 +35,7 @@ public class Listener extends ListenerAdapter {
             org.json.simple.JSONArray messageList = JSONDude.getServerLists();
             if (messageList.size() > 0) {
                 //Get updated server lists message
-                Message message = serverListCommand();
+                Message message = serverListCommand(false);
                 for (Object channelAndMessageIDJSON : messageList) {
                     //Each json array has IDs for messages and the channel they were sent in
                     //converts object to JSONArray
@@ -69,53 +70,176 @@ public class Listener extends ListenerAdapter {
         MessageChannel channel = event.getChannel();
         //split user commands into separate strings
         String[] command = content.split(" ", 4);
-        if (command[0].equalsIgnoreCase("!addlist")) {
-            //check for administrative role
-            boolean roleFound = false;
-            for (Role r : message.getGuild().getMember(message.getAuthor()).getRoles()) {
-                if (r.getName().equalsIgnoreCase("Moderators") || r.getName().equalsIgnoreCase("Sushi Administrators") || r.getName().equalsIgnoreCase("Admin")) {
-                    roleFound = true;
-                    break;
+
+
+        if (event.isFromType(ChannelType.TEXT) || event.isFromType(ChannelType.PRIVATE)) {
+
+
+            if (command[0].equalsIgnoreCase("!addlist")) {
+
+                //check for administrative role
+                boolean roleFound = false;
+                for (Role r : message.getGuild().getMember(message.getAuthor()).getRoles()) {
+                    if (r.getName().equalsIgnoreCase("Moderators") || r.getName().equalsIgnoreCase("Sushi Administrators") || r.getName().equalsIgnoreCase("Admin")) {
+                        roleFound = true;
+                        break;
+                    }
                 }
-            }
-            if (!roleFound) { //stops if administrative role not found
-                return;
-            }
-
-            //Build the initial server list
-            Message initialMessage = serverListCommand();
-
-            //Store id of channel and message
-            channel.sendMessage(initialMessage).queue(t -> { //message callback
-                //get list of IDs from json file and add to them
-                org.json.simple.JSONArray messageList = JSONDude.getServerLists();
-                //new ID pair json list
-                org.json.simple.JSONArray channelAndMessageID = new org.json.simple.JSONArray();
-                channelAndMessageID.add(channel.getId());
-                channelAndMessageID.add(t.getId());
-                messageList.add(channelAndMessageID);
-                //commit to file
-                FileWriter jsonFile = null;
-                try {
-                    jsonFile = new FileWriter("./lists.json");
-                    jsonFile.write(messageList.toJSONString());
-                    jsonFile.flush();
-                    jsonFile.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
+                if (!roleFound) {
+                    return;
                 }
-            });
-            //reminder message. todo: automatically pin the message
-            channel.sendMessage("Please pin the server list! It will update automatically every " + UPDATE_INTERVAL + " minutes.").queue();
 
+                //build the initial server list
+                Message initialMessage = serverListCommand(false);
+
+                //Store id of channel and message
+                channel.sendMessage(initialMessage).queue(t -> { //message callback
+                    //get list of IDs from json file and add to them
+                    org.json.simple.JSONArray messageList = JSONDude.getServerLists();
+                    //new ID pair json list
+                    org.json.simple.JSONArray channelAndMessageID = new org.json.simple.JSONArray();
+                    channelAndMessageID.add(channel.getId());
+                    channelAndMessageID.add(t.getId());
+                    messageList.add(channelAndMessageID);
+                    //commit to file
+                    FileWriter jsonFile = null;
+                    try {
+                        jsonFile = new FileWriter("./lists.json");
+                        jsonFile.write(messageList.toJSONString());
+                        jsonFile.flush();
+                        jsonFile.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+                //reminder message. todo: automatically pin the message
+                channel.sendMessage("Please pin the server list! It will update automatically every " + UPDATE_INTERVAL + " minutes.").queue();
+
+
+            }
+
+
+            if (command[0].equalsIgnoreCase("!ping")) {
+                channel.sendMessage("fuck you").queue();
+
+            } else if (command[0].equalsIgnoreCase("!help")) {
+
+                EmbedBuilder messageReply = new EmbedBuilder();
+                messageReply.setTitle("Message Commands");
+                messageReply.setDescription("");
+                messageReply.setColor(Color.decode("#3EB97E"));
+                messageReply.addField("!ping", "Checks if the bot is online", false);
+                messageReply.addField("!help", "sends help", false);
+                messageReply.addField("!servers", "Lists public Reflex servers with players", false);
+//                messageReply.addField("!sushiservers", "Lists public Sushi ruleset servers with players", false);
+                messageReply.addField("!cointoss <heads/tails>", "Flips a coin (not rigged)", false);
+                messageReply.addField("!notify <NA/EU>", "Gives you pickup role", false);
+                messageReply.addField("!removerole <NA/EU>", "Removes pickup role", false);
+//                messageReply.addField("","",true);
+                channel.sendMessage(messageReply.build()).queue();
+
+                EmbedBuilder sharqCoinReply = new EmbedBuilder();
+                sharqCoinReply.setTitle("SharqCoin Commands");
+                sharqCoinReply.setDescription("");
+                sharqCoinReply.setColor(Color.decode("#F5CA40"));
+                sharqCoinReply.addField("!faq", "Frequently asked questions about sharqcoin", false);
+                sharqCoinReply.addField("!wallet", "View your current sharqcoin balance", false);
+                sharqCoinReply.addField("!send <amount> @user <message>", "Send another user sharqcoin", false);
+                sharqCoinReply.addField("!top5", "forbes list of top billionaires", false);
+                channel.sendMessage(sharqCoinReply.build()).queue();
+
+
+                EmbedBuilder betReply = new EmbedBuilder();
+                betReply.setTitle("Betting Commands");
+                betReply.setColor(Color.decode("#6C0EF7"));
+                betReply.addField("PM the bot: !bet <amount> <player>", "Places a wager on a player who is part of an open bet", false);
+                betReply.addField("!openbets", "Lists current games open for betting", false);
+                betReply.addField("!closedbets", "Lists current in-progress games", false);
+                channel.sendMessage(betReply.build()).queue();
+
+//                EmbedBuilder pickupReply = new EmbedBuilder();
+//                pickupReply.setTitle("Pickup Commands");
+//                pickupReply.setDescription("|");
+//                pickupReply.setColor(Color.decode("#3EB97E"));
+//                pickupReply.addField("@SharqBot add <mode>", "Add yourself to pickup queue for specified mode", false);
+//                pickupReply.addField("@SharqBot remove <mode>", "Remove yourself from specified pickup queue", false);
+//                pickupReply.addField("@SharqBot who", "Displays status of current queues.", false);
+//                pickupReply.addField("@SharqBot start <mode>", "Starts pickup in case you don't want to wait for it to fill up", false);
+//                channel.sendMessage(pickupReply.build()).queue();
+
+            } else if (command[0].equalsIgnoreCase("!cointoss") && ((command[1].equalsIgnoreCase("heads")) || (command[1].equalsIgnoreCase("tails")))) {
+                if (event.isFromType(ChannelType.PRIVATE)) {
+                    return;
+                }
+                EmbedBuilder cointoss = new EmbedBuilder();
+                if (message.getAuthor().getId().equals("95641408530026496")) {
+                    if (command[1].equalsIgnoreCase("heads")) {
+                        cointoss.setTitle("Coin toss result is: heads!");
+
+                    } else {
+                        cointoss.setTitle("Coin toss result is: tails!");
+
+                    }
+                } else {
+                    if (command[1].equalsIgnoreCase("heads")) {
+                        cointoss.setTitle("Coin toss result is: tails!");
+                    } else {
+                        cointoss.setTitle("Coin toss result is: heads!");
+                    }
+                }
+                channel.sendMessage(cointoss.build()).queue();
+
+
+            } else if (command[0].equalsIgnoreCase("!notify")) {
+                if (event.isFromType(ChannelType.TEXT)) {
+                    return;
+                }
+                if (message.getGuild().getId().equals("407749422592819200")) {
+//                    Role NA = message.getGuild().getRolesByName("NA", true).get(0);
+                    Role NA = message.getGuild().getRoleById("411979598541225996");
+//                    Role EU = message.getGuild().getRolesByName("EU", true).get(0);
+                    Role EU = message.getGuild().getRoleById("408771458622291969");
+                    GuildController guildController = new GuildController(message.getGuild());
+
+                    if (command[1].equalsIgnoreCase("NA")) {
+                        guildController.addSingleRoleToMember(message.getMember(), NA).queue();
+                    } else if (command[1].equalsIgnoreCase("EU")) {
+                        guildController.addSingleRoleToMember(message.getMember(), EU).queue();
+                    }
+                }
+
+            } else if (command[0].equalsIgnoreCase("!removeRole")) {
+                if (event.isFromType(ChannelType.TEXT)) {
+                    return;
+                }
+                if (message.getGuild().getId().equals("407749422592819200")) {
+                    Role NA = message.getGuild().getRoleById("411979598541225996");
+                    Role EU = message.getGuild().getRoleById("408771458622291969");
+                    GuildController guildController = new GuildController(message.getGuild());
+
+                    if (command[1].equalsIgnoreCase("NA")) {
+                        guildController.removeSingleRoleFromMember(message.getMember(), NA).queue();
+                    } else if (command[1].equalsIgnoreCase("EU")) {
+                        guildController.removeSingleRoleFromMember(message.getMember(), EU).queue();
+                    }
+                }
+
+            } else if (command[0].equalsIgnoreCase("!servers")/* || command[0].equalsIgnoreCase("!sushiservers")*/) {
+                channel.sendMessage(serverListCommand(true)).queue();
+
+
+            }
         }
-
     }
 
-    private Message serverListCommand() {
+
+    private Message serverListCommand(boolean isFromManualCommand) {
+        boolean sushi = false;
+//                if (command[0].equalsIgnoreCase("!sushiservers")) {
+//                    sushi = true;
+//                }
 
         ArrayList<Server> serverList = new ArrayList<>();
-        //reads from site
         URL syncoreSite;
         try {
             syncoreSite = new URL("https://reflex.syncore.org/api/servers");
@@ -156,7 +280,18 @@ public class Listener extends ListenerAdapter {
                 for (int j = 0; j < playerList.length(); j++) {
                     playerArray.add(playerList.getJSONObject(j).getString("name"));
                 }
-                //create Server object and add it to master array
+
+//                        if (sushi) {
+////                            if ((version.equals("1.1.2expplus")) || version.equals("1.1.4expplus")) {
+////                                Server serverObject = new Server(address, serverName, map, gameTypeShort, players, maxPlayers, playerArray);
+////                                serverList.add(serverObject);
+////                            }
+////                        } else {
+////                            if (!version.equals("1.1.2expplus") && !version.equals("1.1.4expplus")) {
+////                                Server serverObject = new Server(address, serverName, map, gameTypeShort, players, maxPlayers, playerArray);
+////                                serverList.add(serverObject);
+////                            }
+////                        }
                 Server serverObject = new Server(address, serverName, map, gameTypeShort, players, maxPlayers, playerArray);
                 serverList.add(serverObject);
             }
@@ -174,25 +309,27 @@ public class Listener extends ListenerAdapter {
             return new MessageBuilder().setEmbed(new EmbedBuilder().setDescription("No public servers with players :frowning:").setFooter("List last updated on " + timeString + " EST.", "https://reflex.syncore.org/images/reflex.png")
 .build()).build();
         } else {//If any filtered server list isn't empty
-            return new MessageBuilder().setEmbed(buildServerList(serverList)).build();
+            return new MessageBuilder().setEmbed(buildServerList(serverList, sushi, isFromManualCommand)).build();
         }
     }
 
-    private MessageEmbed buildServerList(ArrayList<Server> serverList) {
+    private MessageEmbed buildServerList(ArrayList<Server> serverList, boolean sushi, boolean isFromManualCommand) {
         EmbedBuilder messageReply = new EmbedBuilder();
         messageReply.setTitle("Servers", "https://reflex.syncore.org/");
         messageReply.setDescription("━━━━━━━━━━");
         messageReply.setColor(Color.RED);
         for (Server s : serverList) {
 
-            messageReply.addField("__(" + s.getPlayers() + "/" + s.getMaxPlayers() + ")__ " + s.getGameTypeShort() + " on " + s.getMap(), s.getServerName() + ": steam://connect/" + s.getAddress(), false);
-            /* hyperlink format for when discord supports steam connect hyperlinks
-             */
-            //                messageReply.addField("__(" + s.getPlayers() + "/" + s.getMaxPlayers() + ")__ " + s.getGameTypeShort() + " on " + s.getMap(), "[" + s.getServerName() + "]" + "(steam://connect/" + s.getAddress() + ")", false);
+            if (sushi) {
+                messageReply.addField("(" + s.getPlayers() + "/" + s.getMaxPlayers() + ") " + s.getGameTypeShort() + " on " + s.getMap(), s.getServerName() + "  `connect " + s.getAddress() + "`", false);
 
-            /* legacy console command paste instead of steam connect link
-             */
-            //messageReply.addField("(" + s.getPlayers() + "/" + s.getMaxPlayers() + ") " + s.getGameTypeShort() + " on " + s.getMap(), s.getServerName() + "  `connect " + s.getAddress() + "`", false);
+            } else {
+                messageReply.addField("__(" + s.getPlayers() + "/" + s.getMaxPlayers() + ")__ " + s.getGameTypeShort() + " on " + s.getMap(), s.getServerName() + ": steam://connect/" + s.getAddress(), false);
+
+
+                //hyperlink format for when discord supports steam connect hyperlinks
+                //                messageReply.addField("__(" + s.getPlayers() + "/" + s.getMaxPlayers() + ")__ " + s.getGameTypeShort() + " on " + s.getMap(), "[" + s.getServerName() + "]" + "(steam://connect/" + s.getAddress() + ")", false);
+            }
 
             StringBuilder playerField = new StringBuilder();
             for (String p : s.getPlayerList()) {
@@ -204,12 +341,20 @@ public class Listener extends ListenerAdapter {
             messageReply.addField("Players: ", playerField.toString(), false);
 
         }
-        LocalTime localTime = LocalTime.now();
-        String timeString = localTime.getHour() + ":" + localTime.getMinute();
+        if(isFromManualCommand) {
+            messageReply.setFooter("Bot by Sharqosity. Server data from Syncore.","https://reflex.syncore.org/images/reflex.png");
+
+        } else {
+            LocalTime localTime = LocalTime.now();
+            String timeString = localTime.getHour() + ":" + localTime.getMinute();
 
 //        messageReply.setFooter("Bot by Sharqosity. Server data from Syncore. This list updates every " + UPDATE_INTERVAL + " minutes.", "https://reflex.syncore.org/images/reflex.png");
-        messageReply.setFooter("Bot by Sharqosity. Server data from Syncore. This list last updated on " + timeString + " EST.", "https://reflex.syncore.org/images/reflex.png");
+            messageReply.setFooter("Bot by Sharqosity. Server data from Syncore. This list last updated on " + timeString + " EST.", "https://reflex.syncore.org/images/reflex.png");
+        }
+
 
         return messageReply.build();
+
+//        channel.sendMessage(messageReply.build()).queue();
     }
 }
